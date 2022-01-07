8699492b21a0677928c93ecd6c92ee7940223b65
Daniel <daniel.afilipoaie@gmail.com>
2022-01-07 17:10:01 +0200

    Data structures and operators

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -16,54 +16,236 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  //   openingHours: {
-  //     thu: {
-  //       open: 12,
-  //       close: 22,
-  //     },
-  //     fri: {
-  //       open: 11,
-  //       close: 23,
-  //     },
-  //     sat: {
-  //       open: 0, // Open 24 hours
-  //       close: 24,
-  //     },
-  //   },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orderDelivery: function (starterIndex, mainIndex, address) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at time `
+    );
+  },
 };
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 
-const [x, y, z] = arr;
-console.log(x, y, z);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+//Mutating variable Objects
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log(a, b);
+
+//Nested Objects
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
 // const temp = main;
 // main = secondary;
 // secondary = temp;
 // console.log(main, secondary);
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 // console.log(restaurant.order(2, 0));
 
 //Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(`${starter} and ${mainCourse}`);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(`${starter} and ${mainCourse}`);
 
 //Nested destructuring
-const nested = [2, 4, [5, 6]];
+// const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 // console.log(i, j);
 
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
 
 //Default values
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //Copy array
+
+// const mainManuCopy = [...restaurant.mainMenu];
+// console.log(mainManuCopy);
+
+// //Join 2 arrays
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// //Iterables: arrays, strings,maps, sets. Not Objects
+
+// const str = 'Jonas';
+// const letters = [...str, '', 'S.'];
+// console.log(letters);
+
+// console.log(...str);
+
+// //Objects
+
+// const newRestaurant = {
+//   foundedIn: 1998,
+//   ...restaurant,
+//   founder: 'Giusseppe',
+// };
+
+// console.log(newRestaurant);
+
+// const restaurantCopy = {
+//   ...restaurant,
+// };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+//The rest operator
+
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+
+// console.log(pizza, risotto, otherFood);
+
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+// Functions
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(3, 4, 5, 6, 7, 7, 8);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+//Use any data type, return any data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log(0 && 'jonas');
+// console.log(7 && 'jonas');
+
+// console.log('Hello' && 23 && null && 'jonas');
+
+// if (restaurant.orderPiza) {
+//   restaurant.orderPiza('mushroom', 'spinach');
+// }
+
+// restaurant.orderPiza && restaurant.orderPiza('mushrooms', 'spinach');
+
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// console.log(guests1);
+
+// restaurant.numGuests = 0;
+
+//Nullish: null and undefined (Not 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+
+console.log(guestCorrect);
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// console.log(rest1);
+// console.log(rest2);
+
+//The OR assigment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// console.log(rest1);
+// console.log(rest2);
+
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// console.log(rest1);
+// console.log(rest2);
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+console.log(rest1);
+console.log(rest2);
